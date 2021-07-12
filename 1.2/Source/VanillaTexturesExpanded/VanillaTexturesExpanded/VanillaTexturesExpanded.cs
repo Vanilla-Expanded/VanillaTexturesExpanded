2b714e5e46bd33f2687a8af03ed847d2197febba
Helixien <helixien@gmail.com>
2021-07-13 00:31:56 +0200

    Update for 1.3 part 1
    - Checked patches Oskar said were broken, but they work fine.
    - Added new HD plants from ReGrowth
    - Added patches for Immature textures.
    - Added new HD textures by Isorex

---
+++ 1.2/Source/VanillaTexturesExpanded/VanillaTexturesExpanded/VanillaTexturesExpanded.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using Verse;
+using RimWorld;
+using HarmonyLib;
+
+namespace VanillaTexturesExpanded
+{
+
+    public class VanillaTexturesExpanded : Mod
+    {
+
+        public VanillaTexturesExpanded(ModContentPack content) : base(content)
+        {
+            #if DEBUG
+                Log.Error("Somebody left debugging enabled in Vanilla Textures Expanded - please let the team know!");
+            #endif
+
+            settings = GetSettings<VanillaTexturesExpandedSettings>();
+            harmonyInstance = new Harmony("OskarPotocki.VanillaTexturesExpanded");
+        }
+
+        public override string SettingsCategory() => "VanillaTexturesExpanded.SettingsCategory".Translate();
+
+        public override void DoSettingsWindowContents(Rect inRect)
+        {
+            settings.DoWindowContents(inRect);
+        }
+
+        public static VanillaTexturesExpandedSettings settings;
+        public static Harmony harmonyInstance;
+
+    }
+
+}
